d4b35c6227670944e9ff51431f13868aeaaadec5
Igor Sergeev <igor.v.sergeev@gmail.com>
2017-11-25 15:21:21 -0500

    Map initialization stuff - work in progress

--- src/views/MapView.js
+++ src/views/MapView.js
@@ -1,10 +1,44 @@
 
-import React from 'react'
-import MAPJS from 'mindmup-mapjs'
+import React  from 'react'
+import MAPJS  from 'mindmup-mapjs'
+import jQuery from 'jquery'
+
+import themeProvider from './Theme'
+import testMap       from '../data/sample-map.json'
+import Layout        from 'mindmup-mapjs-layout'
+import Model         from 'mindmup-mapjs-model'
+
+const ThemeProcessor = Layout.ThemeProcessor
+const content        = Model.content
 
 export default class MapView extends React.Component {
+  componentDidMount() {
+    const container = jQuery('#container')
+    const idea      = content(testMap)
+    const mapModel  = new MAPJS.MapModel(MAPJS.DOMRender.layoutCalculator, [])
+
+    jQuery.fn.attachmentEditorWidget = mapModel => {
+      return this.each(function () {
+        mapModel.addEventListener('attachmentOpened', function (nodeId, attachment) {
+          mapModel.setAttachment('attachmentEditorWidget', nodeId, {
+              contentType: 'text/html',
+              content:      window.prompt('attachment', attachment && attachment.content)
+            }
+          )
+        })
+      })
+    }
+    window.onerror = window.alert
+
+    // jQuery('#themecss').themeCssWidget(themeProvider, new ThemeProcessor(), mapModel)
+    // container.domMapWidget(console, mapModel, false, imageInsertController)
+    // jQuery('body').mapToolbarWidget(mapModel)
+    // jQuery('body').attachmentEditorWidget(mapModel)
+    // mapModel.setIdea(idea)
+
+  }
+
   render() {
-    alert(MAPJS)
     return (
       <div>
         <h2>Me is map view</h2>
