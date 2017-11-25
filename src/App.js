edd493a8fa18a4a846a5f41b4fdd1f9399acd3bb
Igor Sergeev <igor.v.sergeev@gmail.com>
2017-11-25 14:41:36 -0500

    Add a map component route

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import './styles/App.css'
 import React from 'react'
 import logo  from './logo.svg'
 
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
+
+import MapView from './views/MapView'
+
 export default class App extends React.Component {
   render() {
     return (
-      <div className="app">
-        <header className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h1 className="app-title">Welcome to React</h1>
-        </header>
-        <p className="app-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div className="app">
+          <Route path="/" render={props => {
+            return (
+              <div>
+                <header className="app-header">
+                  <img src={logo} className="app-logo" alt="logo" />
+                  <h1 className="app-title">Cloud Mind</h1>
+                </header>
+
+                <Route exact path="/" component={MapView} />
+              </div>
+            )
+          }} />
+        </div>
+      </Router>
+    )
   }
 }
---
+++ src/views/MapView.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default class MapView extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Me is map view</h2>
+      </div>
+    )
+  }
+}
