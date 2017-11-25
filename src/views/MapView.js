9c4b04c67c4675b729ad9339628e987882adc8e6
Igor Sergeev <igor.v.sergeev@gmail.com>
2017-11-25 18:45:54 -0500

    More tweaks to get it working

--- src/views/MapView.js
+++ src/views/MapView.js
@@ -4,7 +4,7 @@ import MAPJS  from 'mindmup-mapjs'
 // import jQuery from 'jquery'
 import jQuery from 'mindmup-mapjs/node_modules/jquery'
 
-import themeProvider from './Theme'
+import themeProviderx from './Theme'
 import testMap       from '../data/sample-map.json'
 import Layout        from 'mindmup-mapjs-layout'
 import Model         from 'mindmup-mapjs-model'
@@ -12,11 +12,19 @@ import Model         from 'mindmup-mapjs-model'
 const ThemeProcessor = Layout.ThemeProcessor
 const content        = Model.content
 
+const themeProvider = {
+  'default': themeProviderx
+}
+
 export default class MapView extends React.Component {
   componentDidMount() {
+    this.init()
+  }
+
+  init() {
     const container = jQuery('#container')
-    //const imageInsertController = new MAPJS.ImageInsertController('http://localhost:4999?u=')
     const idea      = content(testMap)
+    const imageInsertController = new MAPJS.ImageInsertController('http://localhost:4999?u=')
     const mapModel  = new MAPJS.MapModel(MAPJS.DOMRender.layoutCalculator, [])
 
     jQuery.fn.attachmentEditorWidget = function (mapModel) {
@@ -32,12 +40,39 @@ export default class MapView extends React.Component {
     }
     window.onerror = window.alert
 
+    themeProvider.default.connector.default.type = 'straight'
+
     jQuery('#themecss').themeCssWidget(themeProvider, new ThemeProcessor(), mapModel)
-    container.domMapWidget(console, mapModel, false/*, imageInsertController*/)
+    container.domMapWidget(console, mapModel, false, imageInsertController)
     jQuery('body').mapToolbarWidget(mapModel)
     jQuery('body').attachmentEditorWidget(mapModel)
     mapModel.setIdea(idea)
 
+    jQuery('#linkEditWidget').linkEditWidget(mapModel)
+    window.mapModel = mapModel
+    jQuery('.arrow').click(function () {
+      jQuery(this).toggleClass('active')
+    })
+    imageInsertController.addEventListener('imageInsertError', function (reason) {
+      console.log('image insert error', reason)
+    })
+
+    container.on('drop', function (e) {
+      const dataTransfer = e.originalEvent.dataTransfer
+      e.stopPropagation()
+      e.preventDefault()
+
+      if (dataTransfer && dataTransfer.files && dataTransfer.files.length > 0) {
+        const fileInfo = dataTransfer.files[0]
+        if (/\.mup$/.test(fileInfo.name)) {
+          const oFReader = new window.FileReader()
+          oFReader.onload = function (oFREvent) {
+            mapModel.setIdea(content(JSON.parse(oFREvent.target.result)))
+          }
+          oFReader.readAsText(fileInfo, 'UTF-8')
+        }
+      }
+    })
   }
 
   render() {
