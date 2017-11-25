d0cbf42d0fe055c323a443064629e556a6923097
Igor Sergeev <igor.v.sergeev@gmail.com>
2017-11-25 15:01:33 -0500

    Add mindmup dependency

--- src/views/MapView.js
+++ src/views/MapView.js
@@ -1,8 +1,10 @@
 
 import React from 'react'
+import MAPJS from 'mindmup-mapjs'
 
 export default class MapView extends React.Component {
   render() {
+    alert(MAPJS)
     return (
       <div>
         <h2>Me is map view</h2>
